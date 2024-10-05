7a75ae613b17ed600d45c18806cc14c4d9e1603f
AyaSasaki0403 <168701750+AyaSasaki0403@users.noreply.github.com>
2024-10-05 20:40:01 +0900

    Add files via upload

---
+++ grad_v1/funcs.php
@@ -0,0 +1,35 @@
+<?php
+
+function h($str){
+
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+// function db_conn(){
+//     try {
+//         $db_name = 'birdies';    //データベース名
+//         $db_id   = 'root';      //アカウント名
+//         $db_pw   = '';      //パスワード
+//         $db_host = 'localhost'; //DBホスト
+//         $pdo = new PDO('mysql:dbname=' . $db_name . ';charset=utf8;host=' . $db_host, $db_id, $db_pw);
+//         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // エラーモードの設定
+//         return $pdo;
+//     } catch (PDOException $e) {
+//         exit('DB Connection Error:' . $e->getMessage());
+//     }
+
+// }
+
+
+
+function sql_error($stmt){
+    $error = $stmt->errorInfo();
+    exit('SQLError:' . print_r($error, true));
+}
+
+function redirect($file_name){
+    header('Location: ' . $file_name);
+    exit();
+}
+
+?>
